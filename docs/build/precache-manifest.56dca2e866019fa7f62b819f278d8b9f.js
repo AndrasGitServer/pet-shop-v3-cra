9979cabaff94578f619fb09c3324eca2a55e36a5
AndrasGitServer <andras_pc@hotmail.com>
2020-03-08 20:46:14 -0400

    initial commit

---
+++ docs/build/precache-manifest.56dca2e866019fa7f62b819f278d8b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cd842c08014d3e8a5476f72e879efa",
+    "url": "/pet-shop-v3-cra/index.html"
+  },
+  {
+    "revision": "9f75915e76b7e13f1f7b",
+    "url": "/pet-shop-v3-cra/static/css/main.e7efc2dc.chunk.css"
+  },
+  {
+    "revision": "11c83f62ef8ddbf77025",
+    "url": "/pet-shop-v3-cra/static/js/2.dcc7a9c2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pet-shop-v3-cra/static/js/2.dcc7a9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f75915e76b7e13f1f7b",
+    "url": "/pet-shop-v3-cra/static/js/main.5a47db62.chunk.js"
+  },
+  {
+    "revision": "0d903660ece0fbbcb17f",
+    "url": "/pet-shop-v3-cra/static/js/runtime-main.575cb8b5.js"
+  }
+]);
